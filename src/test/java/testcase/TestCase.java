fa6ee72b14d35be58671129df0a0bfed0d4c4ec6
htctskn <94467689+htctskn@users.noreply.github.com>
2022-09-23 18:05:36 +0300

    test2

--- src/test/java/testcase/TestCase.java
+++ src/test/java/testcase/TestCase.java
@@ -1,9 +1,12 @@
 package testcase;
 
 import a101page.A101;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import utilities.ConfigReader;
 import utilities.Driver;
+
+import java.util.List;
 
 public class TestCase {
   /*  - Senaryoya üye kaydı oluşturmadan devam edilecek.
@@ -18,10 +21,18 @@ public class TestCase {
             - Siparişi tamamla butonuna tıklayarak, ödeme ekranına gidildiği ,doğru ekrana yönlendiklerini kontrol edecekler.*/
 
 @Test
- public void test(){
+ public void test() throws InterruptedException {
      Driver.getDriver().get(ConfigReader.getProperty("A101URL"));
 
     A101 a101=new A101();
+
+    a101.cerezButton.click();
+    a101.giyimAksesuarBolumu.click();
+    a101.kadinIcGiyimBolumu.click();
+    Thread.sleep(2000);
+    a101.dizAltiCorapBolumu.click();
+   // List<WebElement> corapListesi=a101.tumCoraplar.getText().
+
 
 
 
